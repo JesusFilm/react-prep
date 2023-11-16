df9169b6ad2eb6218979e25ea7ea520fbbb894ec
ax00ng <annaxiong829@gmail.com>
2023-11-16 21:29:47 +0000

    edit

--- jfp-ts-test/src/app/components/Title/Title.tsx
+++ jfp-ts-test/src/app/components/Title/Title.tsx
@@ -1,6 +1,15 @@
-export function Title(label: string) {
+export function Title({label}: string) {
   return(
   <>
-    <h2 className="title">{label}</h2>
+    <h2 className="title">{label?? 'This is a Title'}</h2>
   </>);
+}
+
+
+export function Title({ label }: TitleProps): ReactElement {
+  return (
+    <Typography variant="h1" sx={{ my: 4, textAlign: 'center' }}>
+      {label ?? 'Hello World'}
+    </Typography>
+  )
 }
