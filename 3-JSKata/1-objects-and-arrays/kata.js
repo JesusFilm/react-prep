6bb582c0cbd5d8ffe9fa5760a2fddf17933e4bef
zwsaqx <77838223+zwsaqx@users.noreply.github.com>
2024-01-04 09:41:33 +0000

    part 1 and 2 completed

--- 3-JSKata/1-objects-and-arrays/kata.js
+++ 3-JSKata/1-objects-and-arrays/kata.js
@@ -112,44 +112,108 @@ function hasItem(arr, item) {
 
 // getItemAtIndex should return arr[idx] but only if that index exists:
 // if it doesn't, return a JavaScript Error object.
-function getItemAtIndex(arr, idx) {}
+function getItemAtIndex(arr, idx) {
+  if (arr[idx] == undefined) {
+    return Error() //should put an error message in normally but test requires an error without message.
+  } else {
+    return arr[idx]
+  }
+}
 
 // replaceItemAtIndex should return a copy of `arr` with
 // the element at `idx` replaced with `item`
 // Tip: consider the array literal spread syntax
-function replaceItemAtIndex(arr, idx, item) {}
+function replaceItemAtIndex(arr, idx, item) {
+  const arrCopy = [...arr]
+  arrCopy[idx] = item
+  return arrCopy
+}
 
 // insertItemAtIndex should return a copy of `arr` with `item` inserted at
 // `idx` without overwriting any array values (the array should get longer)
-function insertItemAtIndex(arr, item, idx) {}
+function insertItemAtIndex(arr, item, idx) {
+  const arrCopy = [...arr]
+  arrCopy.splice(idx, 0, item)
+  return arrCopy
+}
 
 // deleteItemAtIndex should return a copy of `arr` without
 // the element at `idx` (the array should get shorter).
-function deleteItemAtIndex(arr, idx) {}
+function deleteItemAtIndex(arr, idx) {
+  const arrCopy = [...arr]
+  delete arrCopy[idx]
+  return arrCopy
+}
 
 // deleteItem should return an array with every instance of `item` removed
-function deleteItem(arr, item) {}
+function deleteItem(arr, item) {
+  const newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== item) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
+}
 
 // zipObject should return an object built from two arrays
 // For example, given ['foo', 'bar'] and [1, 2] it would return
 // { foo: 1, bar: 2 }
-function zipObject(keys, values) {}
+function zipObject(keys, values) {
+  const obj = {}
+  for (let i = 0; i < keys.length; i++) {
+    obj[keys[i]] = values[i]
+  }
+  return obj
+}
 
 // unzipObject should return an array of arrays, each one a pair of keys and values
 // For example, given {foo: 1, bar: 2} it would return
 // [['foo', 1], ['bar', 2]]
-function unzipObject(obj) {}
+function unzipObject(obj) {
+  const arr = []
+  for (let key in obj) {
+    arr.push([key, obj[key]]) //remember to put everything in square brackets so that an array is created in each element of 'arr'
+  }
+  console.log(arr)
+  return arr
+}
 
 // findOneByProperty should return an object from `arr` that has the
 // property AND value of `search`. For example, given:
 //   [{a: 1}, {b: 2, c: 3}] and {b: 2}
 // it will return:
 //   {b: 2, c: 3}
-function findOneByProperty(arr, search) {}
+function findOneByProperty(arr, search) {
+  for (let i = 0; i < arr.length; i++) {
+    const currentObject = arr[i]
+
+    for (let key in search) {
+      if (currentObject[key] == search[key]) {
+        return currentObject
+      }
+    }
+  }
+
+  console.log('not found')
+  return Error()
+}
 
 // findAll should return an array containing all objects in `arr` that
 // have the property and value of `search`
-function findAll(arr, search) {}
+function findAll(arr, search) {
+  const newArray = []
+  for (let i = 0; i < arr.length; i++) {
+    const currentObject = arr[i]
+
+    for (let key in search) {
+      if (currentObject[key] == search[key]) {
+        newArray.push(currentObject)
+      }
+    }
+  }
+  return newArray
+}
 
 module.exports = {
   addName,
--- 3-JSKata/2-strings-and-numbers/utilities.js
+++ 3-JSKata/2-strings-and-numbers/utilities.js
@@ -1,24 +1,70 @@
-function getType(thing) {}
+function getType(thing) {
+  return typeof thing
+}
 
-function isNumber(thing) {}
+function isNumber(thing) {
+  return typeof thing === 'number'
+}
 
-function toNumber(str) {}
+function toNumber(str) {
+  return +str
+}
 
-function isStringNumber(str) {}
+function isStringNumber(str) {
+  return !isNaN(str)
+}
 
-function add(a, b) {}
+function add(a, b) {
+  return a + b
+}
 
-function addStrings(a, b) {}
+function addStrings(a, b) {
+  return String(Number(a) + Number(b))
+}
 
-function addStringsOrNumbers(a, b) {}
+function addStringsOrNumbers(a, b) {
+  if (typeof a == 'string' || typeof b == 'string') {
+    return addStrings(a, b)
+  }
+  return a + b
+}
 
-function isEmail(str) {}
+function isEmail(str) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return emailRegex.test(str)
+}
 
-function countIf(array, fn) {}
+function countIf(array, fn) {
+  let count = 0
+  for (let i = 0; i < array.length; i++) {
+    if (fn(array[i])) {
+      count++
+    }
+  }
+  return count
+}
 
-function filterStringsWithCommas(str) {}
+function filterStringsWithCommas(str) {
+  let commas = 0
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == ',') {
+      commas++
+    }
+  }
 
-function splitStringByCommas(str) {}
+  if (commas == 0) {
+    return false
+  } else if (commas == 1) {
+    return true
+  } else {
+    return commas
+  }
+}
+
+function splitStringByCommas(str) {
+  const newArray = str.split(',')
+  return newArray
+}
 
 module.exports = {
   getType,
