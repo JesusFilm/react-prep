ff74414c34985bc0ac184144fe114c69f1793e53
Edmond Shen <egsmond@gmail.com>
2025-06-06 03:03:37 +0000

    complete getPropertyValue

--- 3-JSKata/1-objects-and-arrays/kata.js
+++ 3-JSKata/1-objects-and-arrays/kata.js
@@ -27,7 +27,9 @@ function makeObject(key, value) {
 
 // getPropertyValue should return the value of the
 // property contained in the `key` of `obj`
-function getPropertyValue(obj, key) {}
+function getPropertyValue(obj, key) {
+  return obj[key]
+}
 
 // addName should return a copy of `obj` with the addition of a `name`
 // property that has the value of the `name` argument
