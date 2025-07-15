c53a99309494f797585f95dfed96009fe40fc3f7
Edmond Shen <egsmond@gmail.com>
2025-07-15 04:08:46 +0000

    complete find

--- 3-JSKata/3-data-structures/find.js
+++ 3-JSKata/3-data-structures/find.js
@@ -1,3 +1,8 @@
-function find(arr, searchDetails) {}
+function find(arr, searchDetails) {
+  return arr.find((element) =>
+    Object.entries(searchDetails).every(
+      ([key, value]) => element[key] === value
+    ))
+}
 
 module.exports = find
