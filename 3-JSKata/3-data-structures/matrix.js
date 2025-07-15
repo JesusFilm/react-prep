308a77fbcdf92b1dac48ed2b3450ee136be2590e
Edmond Shen <egsmond@gmail.com>
2025-07-15 04:08:35 +0000

    complete matrix

--- 3-JSKata/3-data-structures/matrix.js
+++ 3-JSKata/3-data-structures/matrix.js
@@ -1,6 +1,11 @@
-function getMatrix(n) {}
+function getMatrix(n) {
+  return new Array(n).fill(new Array(n).fill(0))
+}
 
-function updateMatrix(matrix, coords, value) {}
+function updateMatrix(matrix, coords, value) {
+  matrix[coords[0]][coords[1]] = value
+  return matrix
+}
 
 module.exports = {
   getMatrix,
