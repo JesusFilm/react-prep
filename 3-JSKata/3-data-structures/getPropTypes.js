16cf800095b7f536b5ecccc36d164ae8794347b8
Edmond Shen <egsmond@gmail.com>
2025-07-15 04:08:22 +0000

    complete getPropTypes

--- 3-JSKata/3-data-structures/getPropTypes.js
+++ 3-JSKata/3-data-structures/getPropTypes.js
@@ -1,3 +1,6 @@
-function getPropTypes(obj) {}
+function getPropTypes(obj) {
+  const values = Object.values(obj)
+  return values.map((value) => { return typeof value})
+}
 
 module.exports = getPropTypes
