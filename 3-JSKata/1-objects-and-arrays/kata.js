d956f4a7244e497ffdb57e66269408fef045ddd9
Vincent Han <hanvincent@outlook.com>
2025-07-21 07:06:47 +0000

    Test 1 Complete

--- 3-JSKata/1-objects-and-arrays/kata.js
+++ 3-JSKata/1-objects-and-arrays/kata.js
@@ -4,46 +4,79 @@
 
 // getGreeting should return a string containing
 // 'Hello ' and the contents of `name`
-function getGreeting(name) {}
+function getGreeting(name) {
+  return 'Hello ' + name
+}
 
 // ageOneYear should return a new object with an `age` property 1 greater
 // than the `age` property of `obj`
-function ageOneYear(obj) {}
+function ageOneYear(obj) {
+  console.log(obj)
+  let newObj = {
+    ...obj,
+    age: obj['age'] + 1,
+  }
+  console.log(newObj)
+
+  return newObj
+}
 
 // makeObject should return an object that looks like this:
 // (but using the arguments passed to the function)
 // {
 //   key: value
 // }
-function makeObject(key, value) {}
+function makeObject(key, value) {
+  return { [key]: value }
+}
 
 // getPropertyValue should return the value of the
 // property contained in the `key` of `obj`
-function getPropertyValue(obj, key) {}
+function getPropertyValue(obj, key) {
+  return obj[key]
+}
 
 // addName should return a copy of `obj` with the addition of a `name`
 // property that has the value of the `name` argument
 // Tip: consider the object literal spread syntax
-function addName(obj, name) {}
+function addName(obj, name) {
+  let newObj = {
+    ...obj,
+    name: name,
+  }
+  return newObj
+}
 
 // deleteProperty should return a new copy of `obj` without the property name
 // that matches the `key` parameter
 // Tip: consider JavaScript's `delete` operator
-function deleteProperty(obj, key) {}
+function deleteProperty(obj, key) {
+  let newObj = { ...obj }
+  delete newObj[key]
+  return newObj
+}
 
 // returnErrorIfFalsy should return a JavaScript Error object with message:
 //   'Oh no, an error!'
 // if val evaluates to false
 // Tip: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
-function returnErrorIfFalsy(val) {}
+function returnErrorIfFalsy(val) {
+  if (val == false) {
+    return Error('Oh no, an error!')
+  }
+}
 
 // keys should return an array of the object's property names (keys)
 // For example, given { foo: 1, bar: 2 } it would return ['foo', 'bar']
-function getKeys(obj) {}
+function getKeys(obj) {
+  return Object.keys(obj)
+}
 
 // getValues should return an array of the object's own values
 // For example, given { foo: 1, bar: 2 } it would return [1, 2]
-function getValues(obj) {}
+function getValues(obj) {
+  return Object.values(obj)
+}
 
 /**
  * Arrays
@@ -52,57 +85,129 @@ function getValues(obj) {}
 // makeArrayOfItem should return an array that is `length` long, made up of
 // `item`. For example, makeArrayOfItem('foo', 2) would return:
 // ['foo', 'foo']
-function makeArrayOfItem(item, length) {}
+function makeArrayOfItem(item, length) {
+  let newArray = []
+  let count = 1
+  while (count <= length) {
+    newArray.push(item)
+    count++
+  }
+  return newArray
+}
 
 // makeArrayOfItems should return an array containing all arguments passed to it
 // Tip: consider JavaScript's Rest parameters
-function makeArrayOfItems() {}
+function makeArrayOfItems() {
+  let newArray = [...arguments]
+  return newArray
+}
 
 // hasItem should return true if `item` is present in `arr` at least once,
 // otherwise it should return false.
 // Tip: there is an array function that makes this straightforward
-function hasItem(arr, item) {}
+function hasItem(arr, item) {
+  return arr.includes(item)
+}
 
 // getItemAtIndex should return arr[idx] but only if that index exists:
 // if it doesn't, return a JavaScript Error object.
-function getItemAtIndex(arr, idx) {}
+function getItemAtIndex(arr, idx) {
+  if (arr[idx]) {
+    return arr[idx]
+  } else {
+    return Error()
+  }
+}
 
 // replaceItemAtIndex should return a copy of `arr` with
 // the element at `idx` replaced with `item`
 // Tip: consider the array literal spread syntax
-function replaceItemAtIndex(arr, idx, item) {}
+function replaceItemAtIndex(arr, idx, item) {
+  let newArr = [...arr]
+  newArr[idx] = item
+  return newArr
+}
 
 // insertItemAtIndex should return a copy of `arr` with `item` inserted at
 // `idx` without overwriting any array values (the array should get longer)
-function insertItemAtIndex(arr, item, idx) {}
+function insertItemAtIndex(arr, item, idx) {
+  let newArr = [...arr]
+  newArr.splice(idx, 0, item)
+  return newArr
+}
 
 // deleteItemAtIndex should return a copy of `arr` without
 // the element at `idx` (the array should get shorter).
-function deleteItemAtIndex(arr, idx) {}
+function deleteItemAtIndex(arr, idx) {
+  let newArr = [...arr]
+  newArr.splice(idx, 1)
+  return newArr
+}
 
 // deleteItem should return an array with every instance of `item` removed
-function deleteItem(arr, item) {}
+function deleteItem(arr, item) {
+  let newArr = []
+  for (i in arr) {
+    if (arr[i] != item) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
+}
 
 // zipObject should return an object built from two arrays
 // For example, given ['foo', 'bar'] and [1, 2] it would return
 // { foo: 1, bar: 2 }
-function zipObject(keys, values) {}
+function zipObject(keys, values) {
+  let newObj = {}
+  for (i in keys) {
+    newObj[keys[i]] = values[i]
+  }
+  return newObj
+}
 
 // unzipObject should return an array of arrays, each one a pair of keys and values
 // For example, given {foo: 1, bar: 2} it would return
 // [['foo', 1], ['bar', 2]]
-function unzipObject(obj) {}
+function unzipObject(obj) {
+  newArr = []
+  objKeys = Object.keys(obj)
+  objValues = Object.values(obj)
+  for (i in objKeys) {
+    let info = [objKeys[i], objValues[i]]
+    newArr.push(info)
+  }
+  return newArr
+}
 
 // findOneByProperty should return an object from `arr` that has the
 // property AND value of `search`. For example, given:
 //   [{a: 1}, {b: 2, c: 3}] and {b: 2}
 // it will return:
 //   {b: 2, c: 3}
-function findOneByProperty(arr, search) {}
+function findOneByProperty(arr, search) {
+  let key = Object.keys(search)[0]
+  let value = search[key]
+  for (obj of arr) {
+    if (obj[key] == value) {
+      return obj
+    }
+  }
+}
 
 // findAll should return an array containing all objects in `arr` that
 // have the property and value of `search`
-function findAll(arr, search) {}
+function findAll(arr, search) {
+  let newArr = []
+  let key = Object.keys(search)[0]
+  let value = search[key]
+  for (obj of arr) {
+    if (obj[key] == value) {
+      newArr.push(obj)
+    }
+  }
+  return newArr
+}
 
 module.exports = {
   addName,
