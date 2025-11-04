d351729920a96703b44646548620855773db4537
JayCHN2004 <jehc055@aucklanduni.ac.nz>
2025-11-04 08:08:06 +0000

    creating dog components

--- 4-React/src/components/Main/Main.jsx
+++ 4-React/src/components/Main/Main.jsx
@@ -1,5 +1,13 @@
 import { Logo } from '../Logo'
+import { Dog } from '../Dog'
 
 export function Main() {
-  return <Logo />
+  return (
+    <div>
+      <Logo />
+      <Dog name="Charles Oliveira" breed="GOAT" superpower="awesome" />
+      <Dog name="Desdemona" breed="Bulldog" superpower="Heat Vision" />
+      <Dog name="test" breed="breed" superpower="superpower" />
+    </div>
+  )
 }
