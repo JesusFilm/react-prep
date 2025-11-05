dba7944ee4b7e0412540e12d2112bcb318b8de2c
DavidLi <dy11222@outloo.com>
2025-11-05 01:28:46 +0000

    Subtitles Implemented for react task

--- 4-React/src/components/Main/Main.jsx
+++ 4-React/src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import { Logo } from '../Logo'
 import { Dog } from '../Dog'
+import { Subtitle } from '../Subtitle'
 
 export function Main() {
   return (
     <div>
       <Logo />
+      <Subtitle Subtitle="Dogs are awsome" />
       <Dog name="Piss" breed="Bulldog" superpower="Heat vision" />
       <Dog name="bob" superpower="yellow piss" />
       <Dog name="bartholomew bartamias the 2nd" breed="Poodle" />
