36f88f178733af182e5384da323708317ad883eb
DavidLi <dy11222@outloo.com>
2025-11-06 07:12:53 +0000

    Array made and passed, unfortunately there is a runtime error

--- 4-React/src/components/DogList/DogList.jsx
+++ 4-React/src/components/DogList/DogList.jsx
@@ -3,10 +3,11 @@ import { Dog } from '../Dog/Dog'
 export function DogList(props) {
   return (
     <div>
+      <span className="dogs">{props.dogs}</span>
       <>
         {props.dogs.map(({ newName, newBreed, newSuperpower }) => (
           <Dog name={newName} breed={newBreed} superpower={newSuperpower}>
-            <img src={'/image/' + newName + '.png'} />
+            <img src={'/image/' + { newBreed } + '.png'} />
           </Dog>
         ))}
       </>
--- 4-React/src/components/Main/Main.jsx
+++ 4-React/src/components/Main/Main.jsx
@@ -1,12 +1,20 @@
 import { Logo } from '../Logo'
 import { Dog } from '../Dog'
 import { Subtitle } from '../Subtitle'
+import { DogList } from '../DogList'
+
+const dogs = [
+  { name: 'Matthew', breed: 'hound', superpower: 'Pyro' },
+  { name: 'Mark', breed: 'bull-terrier', superpower: 'Hydro' },
+  { name: 'Luke', breed: 'greyhound', superpower: 'Electro' },
+  { name: 'John', breed: 'dachshund', superpower: 'Magneto' },
+]
 
 export function Main() {
   return (
     <div>
       <Logo />
-      <Subtitle Subtitle="Dogs are awsome" />
+      <Subtitle Subtitle="Dogs are awesome" />
       <Dog name="Tom" breed="Bulldog" superpower="Heat vision">
         <img src={'/images/bulldog.png'} />
       </Dog>
@@ -20,6 +28,8 @@ export function Main() {
       >
         <img src={'/images/dachshund.png'} />
       </Dog>
+
+      <DogList dogs={dogs} />
     </div>
   )
 }
