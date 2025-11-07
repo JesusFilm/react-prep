66e95df82816d82536eec967d11e2237e966ee66
Edmond Shen <egsmond@gmail.com>
2025-11-07 00:27:10 +0000

    feat: add mock data

---
+++ jfp-ts-test/src/libs/data.ts
@@ -0,0 +1,56 @@
+export enum TimerState {
+  STARTED = 'started',
+  PAUSED = 'paused',
+  STOPPED = 'stopped',
+  COMPLETED = 'completed',
+}
+
+// cspell:disable-next-line
+export interface PomodoroTimer {
+  id: string
+  label: string
+  state: TimerState
+  durationMinutes: number
+  remainingSeconds: number
+  breakDurationMinutes: number
+  breakEverySessions: number
+  completedSessions: number
+  totalSessions: number
+}
+
+// cspell:disable-next-line
+export const timers: PomodoroTimer[] = [
+  {
+    id: '1',
+    label: 'Deep Work Block',
+    durationMinutes: 50,
+    remainingSeconds: 50 * 60,
+    state: TimerState.STARTED,
+    breakDurationMinutes: 5,
+    breakEverySessions: 1,
+    completedSessions: 0,
+    totalSessions: 2,
+  },
+  {
+    id: '2',
+    label: 'Writing Sprint',
+    durationMinutes: 25,
+    remainingSeconds: 15 * 60,
+    state: TimerState.PAUSED,
+    breakDurationMinutes: 5,
+    breakEverySessions: 1,
+    completedSessions: 1,
+    totalSessions: 4,
+  },
+  {
+    id: '3',
+    label: 'Team Planning',
+    durationMinutes: 45,
+    remainingSeconds: 0,
+    state: TimerState.COMPLETED,
+    breakDurationMinutes: 5,
+    breakEverySessions: 2,
+    completedSessions: 2,
+    totalSessions: 2,
+  },
+]
