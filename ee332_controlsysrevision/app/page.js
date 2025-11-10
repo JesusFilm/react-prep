50725b8b01c23c42fc39497e58b3f9e263709b7d
DavidLi <dy11222@outloo.com>
2025-11-10 02:32:31 +0000

    css tailwind configured, first working text implemented

--- ee332_controlsysrevision/app/page.js
+++ ee332_controlsysrevision/app/page.js
@@ -1,10 +1,15 @@
 import Image from 'next/image'
 import { Main } from '@/components/Main'
+import { MainText } from '@/components/MainText'
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center">
       <Main />
+      <MainText
+        paragraph="A way of representing a control systems (especially when it’s output feeds back into the input as an error) is through block diagrams. This is a visual way of seeing the relationships between transfer functions and inputs and outputs.
+"
+      />
     </div>
   )
 }
