c38b2f93808b7deb5baa2bcdedddfd07d18dc52a
DavidLi <dy11222@outloo.com>
2025-11-14 05:49:53 +0000

    Text and Image Formatting introduced. BulletPoints added. I'm going to try and add Latex to this now

--- ee332_controlsysrevision/app/page.js
+++ ee332_controlsysrevision/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Main } from '@/components/Main'
 import { MainText } from '@/components/MainText'
+import { BulletPoint } from '@/components/BulletPoint'
 
 export default function Home() {
   return (
@@ -17,6 +18,16 @@ export default function Home() {
           width={400}
           height={100}
         />
+      </div>
+
+      <div className="grid justify-self-start">
+        <BulletPoint
+          Points="It takes X(s) as the input (or “Cause“) and produces Y(s) as the
+          output (or “Effect“)"
+        />
+        <BulletPoint Points="The block diagram is unidirectional, meaning that the Effect can not produce a Cause" />
+
+        <BulletPoint Points="Mathematically this block looks like: Y(s)=G(s)X(s)" />
       </div>
 
       <div className="grid h-56 grid-cols-2 content-around">
