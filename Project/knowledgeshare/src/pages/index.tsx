d1a86f0ccc5777b5ffd2eb067a1c608c910a515b
Edmond Shen <egsmond@gmail.com>
2025-11-17 00:21:41 +0000

    add comment

--- Project/knowledgeshare/src/pages/index.tsx
+++ Project/knowledgeshare/src/pages/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center gap-4 bg-zinc-50 dark:bg-black">
+  {/* hello world */}
       <Link
         href="/useState"
         className="rounded-md bg-black px-4 py-2 text-white transition-colors hover:bg-zinc-800 dark:bg-white dark:text-black dark:hover:bg-zinc-200"
