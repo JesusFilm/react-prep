3de032b0de0380f479457cb3513e31210f39b8f4
jaycnz <ijaygt@gmail.com>
2025-11-17 03:41:43 +0000

    render timers

--- Project/jfp-ts-test/src/components/Main/Main.tsx
+++ Project/jfp-ts-test/src/components/Main/Main.tsx
@@ -2,7 +2,6 @@ import { Form } from '../Form'
 import { TimerModel, timers } from '../../libs/data'
 import { Timer } from '../Timer'
 import { v4 as uuidv4 } from 'uuid'
-import { Roboto_Flex } from 'next/font/google'
 
 export function Main() {
   const timerList = timers.map((x) => (
