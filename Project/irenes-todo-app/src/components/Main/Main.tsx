c7a12b133993281554f6e35809f8ff8fefb4945c
irenekim21 <irenekimofficial03>
2025-12-01 01:21:15 +0000

    watchpack config

--- Project/irenes-todo-app/src/components/Main/Main.tsx
+++ Project/irenes-todo-app/src/components/Main/Main.tsx
@@ -1,31 +1,18 @@
 'use client'
 
-import { FormControlLabel, Switch } from '@mui/material'
 import { tasks } from '../../libs/data'
 import { Form } from '../Form'
 import { TaskCard } from '../TaskCard'
-import { useState } from 'react'
+
 import { v4 } from 'uuid'
 
 export function Main() {
-  const [checkedstate, setCheckedstate] = useState(false)
-  console.log(checkedstate)
-
   return (
     <>
       <Form />
       {tasks.map((currentTask) => (
         <TaskCard key={v4()} task={currentTask} />
       ))}
-      <FormControlLabel
-        control={
-          <Switch
-            onChange={() => setCheckedstate((prev) => !prev)}
-            checked={checkedstate}
-          />
-        }
-        label="Switch"
-      />
     </>
   )
 }
